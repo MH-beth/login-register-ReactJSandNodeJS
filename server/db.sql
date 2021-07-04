cab2f90e35f52a7ba984549a8385bfdb83a21dad
MH15O <76598767+MH15O@users.noreply.github.com>
2021-07-04 18:44:21 +0100

    Update db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,10 +1,82 @@
-CREATE TABLE `users`(
-    `id` int(11) NOT NULL,
-    `username` varchar(52) NOT NULL,
-    `password` varchar(52) NOT NULL
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 04 juil. 2021 à 19:43
+-- Version du serveur :  10.4.18-MariaDB
+-- Version de PHP : 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `loginsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(52) NOT NULL,
+  `password` varchar(52) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `users` 
-    ADD PRIMARY KEY(`id`);
-ALTER TABLE `Users`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'MH15O', 'theboss1973.0'),
+(2, 'MH16O', 'theboss1973.0'),
+(3, 'MH15O', 'theboss1973.0'),
+(4, 'MH16O', 'theboss1973.0'),
+(5, 'zadazd', 'adzazda'),
+(6, 'zadazd', 'adzazda'),
+(7, 'zadazd', 'adzazda'),
+(8, 'zadazd', 'adzazda'),
+(9, 'zadazd', 'adzazda'),
+(10, 'zadazd', 'adzazda'),
+(11, 'zadazd', 'adzazda'),
+(12, 'zadazd', 'adzazda'),
+(13, 'MH19O', 'theboss1973.0'),
+(14, 'MH20O', 'theboss1973.0'),
+(15, 'MH19Os', 'theboss1973.0'),
+(16, 'azdazd', ''),
+(17, '', '');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
